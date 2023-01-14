d78cda43729966f56f607c9d41ab9756f0d0d276
mackee <macopy123@gmail.com>
2023-01-14 14:59:53 +0900

    やっぱ直接比較する 不等号をミスる

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -2233,7 +2233,7 @@ func postLogin(w http.ResponseWriter, r *http.Request) {
 	hashedPassword := u.HashedPassword
 	var rehashedPassword []byte
 	var rehashed bool
-	if err := dbx.Get(&rehashedPassword, "SELECT hashed_password FROM user_password WHERE user_id = ?", u.ID); err != nil && err == sql.ErrNoRows {
+	if err := dbx.Get(&rehashedPassword, "SELECT hashed_password FROM user_password WHERE user_id = ?", u.ID); err != nil && err != sql.ErrNoRows {
 		log.Print(err)
 
 		outputErrorMsg(w, http.StatusInternalServerError, "db error")
