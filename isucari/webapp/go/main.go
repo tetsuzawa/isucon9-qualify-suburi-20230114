2f8969eca5ac8102c79d1babac743333dab78743
mackee <macopy123@gmail.com>
2023-01-14 17:49:27 +0900

    on saleだけ表示してみる

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -682,9 +682,9 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := dbx.Select(&items,
-			"SELECT * FROM items WHERE status IN (?,?) AND (created_at < ?  OR (created_at <= ? AND id < ?)) ORDER BY created_at DESC, id DESC LIMIT ?",
+			"SELECT * FROM items WHERE status IN (?) AND (created_at < ?  OR (created_at <= ? AND id < ?)) ORDER BY status created_at DESC, id DESC LIMIT ?",
 			ItemStatusOnSale,
-			ItemStatusSoldOut,
+			//ItemStatusSoldOut,
 			time.Unix(createdAt, 0),
 			time.Unix(createdAt, 0),
 			itemID,
@@ -698,9 +698,9 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// 1st page
 		err := dbx.Select(&items,
-			"SELECT * FROM items WHERE status IN (?,?) ORDER BY created_at DESC, id DESC LIMIT ?",
+			"SELECT * FROM items WHERE status IN (?) ORDER BY created_at DESC, id DESC LIMIT ?",
 			ItemStatusOnSale,
-			ItemStatusSoldOut,
+			//ItemStatusSoldOut,
 			ItemsPerPage+1,
 		)
 		if err != nil {
